7c05e5959ad1e84ae2a33732e6a25bee84dfe1e3
Jennifer Chamorro <jecris.chamo@gmail.com>
2025-11-05 11:52:55 -0500

    se corrigen rutas

--- routes/cita.routes.js
+++ routes/cita.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const citaController = require("../controllers/cita.controller");
 
-router.post("/citas", citaController.createCita);
-router.get("/citas", citaController.getAllCitas);
-router.get("/citas/:id", citaController.getCitaById);
-router.put("/citas/:id", citaController.updateCita);
-router.delete("/citas/:id", citaController.deleteCita);
+router.post("/", citaController.createCita);
+router.get("/", citaController.getAllCitas);
+router.get("/:id", citaController.getCitaById);
+router.put("/:id", citaController.updateCita);
+router.delete("/:id", citaController.deleteCita);
 
 module.exports = router;
